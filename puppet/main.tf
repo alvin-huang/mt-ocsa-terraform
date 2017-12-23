71fc1ede74e6d7ba661a5e1be1d1d7b1d4c01241
alexcreek <me@alexcreek.com>
2017-12-23 07:20:07 +0000

    add webhook security group

--- puppet/main.tf
+++ puppet/main.tf
@@ -15,6 +15,18 @@ resource "aws_security_group" "security_group_puppet" {
   }
 }
 
+resource "aws_security_group" "security_group_puppet_webhook" {
+  name        = "${lookup(var.security_group_puppet_webhook, var.region)}"
+  description = "Allow inbound webhooks to the puppet master "
+
+  ingress {
+    from_port   = 0
+    to_port     = 8000
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "puppet_master" {
   ami             = "${lookup(var.ami, var.region)}"
   instance_type   = "t2.medium"
@@ -29,10 +41,10 @@ resource "aws_instance" "puppet_db" {
   security_groups = ["${lookup(var.security_group_puppet, var.region)}"]
 }
 
-resource "aws_eip" "puppet_master_ip" {
+resource "aws_eip" "puppet_master" {
   instance = "${aws_instance.puppet_master.id}"
 }
 
-resource "aws_eip" "puppet_db_ip" {
+resource "aws_eip" "puppet_db" {
   instance = "${aws_instance.puppet_db.id}"
 }
