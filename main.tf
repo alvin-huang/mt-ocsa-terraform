2cd0afdd1997b57bb457665d3878fdb2045357cc
alexcreek <me@alexcreek.com>
2017-12-23 05:56:59 +0000

    enable remote state in s3

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "mtscloudcop-tf-state"
+    key    = "puppet/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config {
+    bucket = "mtscloudcop-tf-state"
+    key    = "puppet/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 module "puppet" {
   source = "./puppet"
 }
