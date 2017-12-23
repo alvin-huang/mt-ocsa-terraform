eee914ec76b25bafdafc8bcb35a86b46012ffc34
alexcreek <me@alexcreek.com>
2017-12-23 07:43:31 +0000

    add jenkins outputs

--- puppet/outputs.tf
+++ puppet/outputs.tf
@@ -5,3 +5,11 @@ output "puppet_master" {
 output "puppet_db" {
   value = "${aws_eip.puppet_db.public_ip}"
 }
+
+output "jenkins_master" {
+  value = "${aws_eip.jenkins_master.public_ip}"
+}
+
+output "jenkins_slave" {
+  value = "${aws_eip.jenkins_slave.public_ip}"
+}
