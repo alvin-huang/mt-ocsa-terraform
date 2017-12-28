eae4538b7dda64283c5a016fa006c3cbea0ef461
Alvin Huang <alvin.huang@fireeye.com>
2017-12-28 01:47:06 -0500

    clean up terraform file formatting

--- puppet/main.tf
+++ puppet/main.tf
@@ -5,19 +5,20 @@ provider "aws" {
 
 # vpcs
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name = "${whatevername}"
-  cidr = "172.16.0.0/16"
-  azs             = ["${var.region}a"]
-  public_subnets  = ["172.16.1.0/24"]
+  source               = "terraform-aws-modules/vpc/aws"
+  name                 = "puppet"
+  cidr                 = "172.16.0.0/16"
+  azs                  = ["${var.region}a"]
+  public_subnets       = ["172.16.1.0/24"]
   enable_dns_support   = true
   enable_dns_hostnames = true
   enable_nat_gateway   = true
 }
 
 resource "aws_vpc_dhcp_options" "puppet" {
-  domain_name = "local"
+  domain_name         = "local"
   domain_name_servers = ["AmazonProvidedDNS"]
+
   tags {
     Name = "puppet"
   }
@@ -29,7 +30,7 @@ resource "aws_vpc_dhcp_options_association" "domain_name" {
 }
 
 resource "aws_route53_zone" "local" {
-  name = "local"
+  name   = "local"
   vpc_id = "${module.vpc.vpc_id}"
 }
 
@@ -44,18 +45,19 @@ resource "aws_route53_record" "puppet_01" {
   ]
 }
 
-
 # security groups
 resource "aws_security_group" "outbound" {
   name        = "outbound"
   description = "Allow outbound traffic"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id      = "${module.vpc.vpc_id}"
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags {
     Name = "outbound"
   }
@@ -64,13 +66,15 @@ resource "aws_security_group" "outbound" {
 resource "aws_security_group" "ssh" {
   name        = "ssh"
   description = "Allow inbound ssh"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id      = "${module.vpc.vpc_id}"
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags {
     Name = "ssh"
   }
@@ -79,13 +83,15 @@ resource "aws_security_group" "ssh" {
 resource "aws_security_group" "webhook" {
   name        = "webhook"
   description = "Allow inbound webhooks to the puppet master "
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id      = "${module.vpc.vpc_id}"
+
   ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags {
     Name = "webhook"
   }
@@ -94,13 +100,15 @@ resource "aws_security_group" "webhook" {
 resource "aws_security_group" "https" {
   name        = "https"
   description = "Allow inbound https"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id      = "${module.vpc.vpc_id}"
+
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags {
     Name = "https"
   }
@@ -109,13 +117,15 @@ resource "aws_security_group" "https" {
 resource "aws_security_group" "puppet" {
   name        = "puppet"
   description = "Allow inbound puppet traffic"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id      = "${module.vpc.vpc_id}"
+
   ingress {
     from_port       = 8140
     to_port         = 8140
     protocol        = "TCP"
     security_groups = ["${aws_security_group.ssh.id}"]
   }
+
   tags {
     Name = "puppet"
   }
@@ -123,24 +133,28 @@ resource "aws_security_group" "puppet" {
 
 # ec2 instances
 resource "aws_instance" "puppet_01" {
-  ami             = "${lookup(var.ami, var.region)}"
-  instance_type   = "t2.medium"
-  key_name        = "acreek"
-  monitoring      = true
-  subnet_id       = "${element(module.vpc.public_subnets, 0)}"
+  ami                    = "${lookup(var.ami, var.region)}"
+  instance_type          = "t2.medium"
+  key_name               = "acreek"
+  monitoring             = true
+  subnet_id              = "${element(module.vpc.public_subnets, 0)}"
   vpc_security_group_ids = ["${aws_security_group.ssh.id}", "${aws_security_group.puppet.id}", "${aws_security_group.webhook.id}", "${aws_security_group.https.id}", "${aws_security_group.outbound.id}"]
+
   root_block_device {
     volume_type           = "gp2"
     volume_size           = 50
     delete_on_termination = true
   }
+
   tags {
     Name = "puppet-01"
   }
+
   provisioner "remote-exec" {
     inline = [
-      "sudo hostnamectl set-hostname --static ${aws_instance.puppet_01.tags.Name}.local"
+      "sudo hostnamectl set-hostname --static ${aws_instance.puppet_01.tags.Name}.local",
     ]
+
     connection {
       type = "ssh"
       user = "centos"
@@ -149,24 +163,28 @@ resource "aws_instance" "puppet_01" {
 }
 
 resource "aws_instance" "jenkins_01" {
-  ami             = "${lookup(var.ami, var.region)}"
-  instance_type   = "t2.medium"
-  key_name        = "acreek"
-  monitoring      = true
-  subnet_id       = "${element(module.vpc.public_subnets, 0)}"
+  ami                    = "${lookup(var.ami, var.region)}"
+  instance_type          = "t2.medium"
+  key_name               = "acreek"
+  monitoring             = true
+  subnet_id              = "${element(module.vpc.public_subnets, 0)}"
   vpc_security_group_ids = ["${aws_security_group.ssh.id}", "${aws_security_group.https.id}", "${aws_security_group.outbound.id}"]
+
   root_block_device {
     volume_type           = "gp2"
     volume_size           = 50
     delete_on_termination = true
   }
+
   tags {
     Name = "jenkins-01"
   }
+
   provisioner "remote-exec" {
     inline = [
-      "sudo hostnamectl set-hostname --static ${aws_instance.jenkins_01.tags.Name}.local"
+      "sudo hostnamectl set-hostname --static ${aws_instance.jenkins_01.tags.Name}.local",
     ]
+
     connection {
       type = "ssh"
       user = "centos"
@@ -175,23 +193,29 @@ resource "aws_instance" "jenkins_01" {
 }
 
 resource "aws_instance" "jenkins_slave_01" {
-  ami             = "${lookup(var.ami, var.region)}"
-  instance_type   = "t2.small"
-  key_name        = "acreek" monitoring      = true
-  subnet_id       = "${element(module.vpc.public_subnets, 0)}"
+  ami           = "${lookup(var.ami, var.region)}"
+  instance_type = "t2.small"
+  key_name      = "acreek"
+
+  monitoring             = true
+  subnet_id              = "${element(module.vpc.public_subnets, 0)}"
   vpc_security_group_ids = ["${aws_security_group.ssh.id}", "${aws_security_group.outbound.id}"]
+
   root_block_device {
     volume_type           = "gp2"
     volume_size           = 50
     delete_on_termination = true
   }
+
   tags {
     Name = "jenkins-slave-01"
   }
+
   provisioner "remote-exec" {
     inline = [
-      "sudo hostnamectl set-hostname --static ${aws_instance.jenkins_slave_01.tags.Name}.local"
+      "sudo hostnamectl set-hostname --static ${aws_instance.jenkins_slave_01.tags.Name}.local",
     ]
+
     connection {
       type = "ssh"
       user = "centos"
@@ -202,10 +226,10 @@ resource "aws_instance" "jenkins_slave_01" {
 # elastic ips
 resource "aws_eip" "puppet_01" {
   instance = "${aws_instance.puppet_01.id}"
-  vpc = true
+  vpc      = true
 }
 
 resource "aws_eip" "jenkins_01" {
   instance = "${aws_instance.jenkins_01.id}"
-  vpc = true
+  vpc      = true
 }
--- variables.tf
+++ variables.tf
@@ -4,6 +4,7 @@ variable "region" {
 
 variable "ami" {
   type = "map"
+
   default = {
     "us-east-1" = "ami-db48ada1"
     "us-east-2" = "ami-d61133b3"
